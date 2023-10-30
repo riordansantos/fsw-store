6b860b42dde391e1c76ff3a8b24754021f57baad
riordan.santos <riordan3607@gmail.com>
2023-10-30 10:35:42 -0300

    feat: add first banner to homepage

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+
+export default function Home() {
+  const { data } = useSession();
+
+  return (
+    <div className="p-5">
+      <Image
+        src="/banner-home-01.png"
+        className="h-auto w-full"
+        sizes="100vw"
+        height={0}
+        width={0}
+        alt="Até 55% de desconto esse mês"
+      />
+    </div>
+  );
+}
