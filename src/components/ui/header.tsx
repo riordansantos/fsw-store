e9371731f3646d2a2b55d68769287f59526af452
riordan.santos <riordan3607@gmail.com>
2023-10-31 11:45:49 -0300

    feat: ensure user goes to catalog page when click on catalog button

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -10,10 +10,17 @@ import {
 } from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
-import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from "./sheet";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
 import { Separator } from "./separator";
+import Link from "next/link";
 
 export default function Header() {
   const handleLoginClick = async () => {
@@ -38,7 +45,6 @@ export default function Header() {
           </SheetHeader>
           {status === "authenticated" && data?.user && (
             <div className="flex flex-col">
-              
               <div className="flex items-center gap-2 py-4">
                 <Avatar>
                   <AvatarFallback>
@@ -80,9 +86,16 @@ export default function Header() {
             <Button variant="outline" className="w-full justify-start gap-2">
               <PercentIcon size="16" /> Ofertas
             </Button>
-            <Button variant="outline" className="w-full justify-start gap-2">
-              <ListOrderedIcon size="16" /> Catálogo
-            </Button>
+            <SheetClose asChild>
+              <Link href="/catalog">
+                <Button
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <ListOrderedIcon size="16" /> Catálogo
+                </Button>
+              </Link>
+            </SheetClose>
           </div>
         </SheetContent>
       </Sheet>
